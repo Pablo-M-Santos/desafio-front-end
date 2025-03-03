f124c9e5e857d810ffbd0611d0d6d8e72559cacf
Pablo <pablomoreirasantos.hp@gmail.com>
2025-03-03 19:32:33 -0300

    style: alteração na cor de fundo do projeto #011

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Table from "../components/Table/Table";
 import Navbar from "../components/Navbar/Navbar";
 const Home: React.FC = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: "#F0F0F0", height: "100vh" }}>
       <Navbar />
       <Table />
     </div>
